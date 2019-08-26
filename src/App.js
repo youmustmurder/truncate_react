4046c92f80185a39c21d6138f38fedb2e66637e8
Slava <slava.ustinow@gmail.com>
2019-08-26 15:06:25 +0500

    feat: add support ie11

--- src/App.js
+++ src/App.js
@@ -6,38 +6,44 @@ import Truncate from './Truncate';
 import './App.css';
 
 class App extends React.Component {
-  state = {
-    truncate: false,
-    expand: false
-  };
-  toggleTruncate = () => {
-    this.setState(state => ({
-      expand: !state.expand
-    }));
-  }
-  onTruncate = (isTruncated) => {
-    this.setState({
-      truncate: isTruncated,
-      expand: false
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Truncate
-          truncate={!this.state.expand}
-          countLines="4"
-          onResult={this.onTruncate}>
-          Lorem <Tag type='red'>ipsum</Tag>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, <Tag type='red'>sunt in culpa qui officia</Tag> deserunt mollit anim id est laborum.
-        </Truncate>
-        {
-          this.state.truncate && (
-            <Button onClick={this.toggleTruncate}>Show/hide</Button>  
-          )
-        }
-      </div>
-    );
-  }
+	state = {
+		truncate: false,
+		expand: false
+	};
+	toggleTruncate = () => {
+		this.setState(state => ({
+			expand: !state.expand
+		}));
+	}
+	onTruncate = (isTruncated) => {
+		if (isTruncated !== this.state.truncate) {
+			this.setState({
+				truncate: isTruncated,
+				expand: false
+			});
+		}
+	}
+	render() {
+		return (
+			<div>
+				{!this.state.expand ? (
+					<Truncate
+						countLines="2"
+						ellipsisChars={"..."}
+						onResult={this.onTruncate}>
+						Lorem <Tag type='red'>ipsum</Tag>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, <Tag type='red'>sunt in culpa qui officia</Tag> deserunt mollit anim id est laborum.
+					</Truncate>
+				) : (
+					<React.Fragment>Lorem <Tag type='red'>ipsum</Tag>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, <Tag type='red'>sunt in culpa qui officia</Tag> deserunt mollit anim id est laborum.</React.Fragment>
+				)}
+				{
+					this.state.truncate && (
+						<Button onClick={this.toggleTruncate}>Show/hide</Button>  
+					)
+				}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Truncate/index.jsx
+++ src/Truncate/index.jsx
@@ -6,8 +6,21 @@ class Truncate extends React.Component {
         super(props);
         this.containerRef = React.createRef();
         this.debounceResize = debounce(this.onResult, 200);
+        this.isSupportNativeClamp = false;
+
+        this.text = "";
+        this.lineHeight = 0;
+
+        this.splitOnChars = [".", "-", "–", "—", " "];
+        this.splitChar = this.splitOnChars[0];
+        this.chunks = null;
+        this.lastChunk = null;
+
+        this.truncatePollyfill = false;
     }
     componentDidMount() {
+        this.isSupportNativeClamp = "webkitLineClamp" in document.body.style;
+        this.text = this.containerRef.current.innerHTML;
         this.processTruncate();
         this.onResult();
         window.addEventListener("resize", this.debounceResize, false);
@@ -19,19 +32,70 @@ class Truncate extends React.Component {
         window.removeEventListener("resize", this.debounceResize, false);
     }
     processTruncate = () => {
-        const style = this.containerRef.current.style;
-        if (this.props.truncate) {
+        if (this.isSupportNativeClamp) {
+            const style = this.containerRef.current.style;
             style.overflow = "hidden";
             style.textOverflow = "ellipsis";
             style.webkitBoxOrient = "vertical";
             style.display = "-webkit-box";
             style.webkitLineClamp = this.props.countLines;
         } else {
-            style.webkitLineClamp = "unset";
+            this.lineHeight = this.getLineHeight();
+            if (this.containerRef.current.offsetHeight > this.lineHeight * this.props.countLines) {
+                this.truncatePollyfill = true;
+                this.truncate();
+            }
         }
     }
     onResult = () => {
-        this.props.onResult(this.containerRef.current.scrollWidth > this.containerRef.current.clientWidth || this.containerRef.current.scrollHeight > this.containerRef.current.clientHeight);
+        if (this.isSupportNativeClamp) {
+            this.props.onResult(this.containerRef.current.scrollWidth > this.containerRef.current.clientWidth || this.containerRef.current.scrollHeight > this.containerRef.current.clientHeight);
+        } else {
+            this.props.onResult(this.truncatePollyfill);
+        }
+    }
+    truncate = () => {
+        if (!this.chunks) {
+            if (this.splitOnChars.length > 0) {
+                this.splitChar = this.splitOnChars.shift();
+            } else {
+                this.splitChar = "";
+            }
+            this.chunks = this.containerRef.current.innerHTML.split(this.splitChar);
+        }
+      
+        if (this.chunks.length > 1) {
+            this.lastChunk = this.chunks.pop();
+            this.containerRef.current.innerHTML = this.chunks.join(this.splitChar) + this.props.ellipsisChars;
+        } else {
+            this.chunks = null;
+        }
+
+        if (this.chunks) {
+            if (this.containerRef.current.offsetHeight <= this.lineHeight * this.props.countLines) {
+                if (this.splitOnChars.length >= 0 && this.splitChar !== "") {
+                    this.containerRef.current.innerHTML = this.chunks.join(this.splitChar) + this.splitChar + this.lastChunk;
+                    this.chunks = null;
+                } else {
+                    return;
+                }
+            }
+        } else if (this.splitChar === "") {
+            return;
+        }
+      
+        this.truncate();
+    }
+    getLineHeight = () => {
+        const lineHeight = this.сomputedStyle("line-height");
+
+        if (lineHeight === "normal") {
+            return Math.ceil(parseFloat(this.сomputedStyle("font-size")) * 1.2);
+        }
+        return Math.ceil(parseFloat(lineHeight));
+    }
+    сomputedStyle = (styleName) => {
+        return document.defaultView.getComputedStyle(this.containerRef.current, null).getPropertyValue(styleName);
     }
     render() {
         return (
